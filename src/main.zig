fe45cdf3dd3d49a6048ec8ec56d0d1afe3a41969
Waseem Ahmed <codewaseem@gmail.com>
2024-12-22 19:59:49 +0530

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -88,9 +88,11 @@ pub fn main() !void {
             std.debug.print("Error: {}\n", .{err});
             std.process.exit(65);
         };
-        try interpreter.run(statements);
-        if (interpreter.runtime_error) {
-            std.process.exit(70);
-        }
+        try interpreter.run(statements) catch |err| {
+            if (interpreter.runtime_error) {
+                std.process.exit(70);
+            }
+            return err;
+        };
     }
 }
